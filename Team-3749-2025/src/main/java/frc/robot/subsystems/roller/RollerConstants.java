768ed962b3625be3a7a1bb4905c4c1be380b06b5
LiliWuu <lilianwu08@gmail.com>
2025-01-14 23:56:37 -0800

    roller sim

--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/RollerConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/RollerConstants.java
@@ -9,17 +9,21 @@ public class RollerConstants {
     public static final int coralMotorId = 1; // example
     public static final int scoringMotorId = 2; // example
 
+    public static final double algaeGearRatio = 1; // example
+    public static final double coralGearRatio = 1; // example
+    public static final double scoringGearRatio = 1; // example
+
     // example PID Sim Values
-    public static double kSAlgaeSim = 0.0;
-    public static double kVAlgaeSim = 0.0;
+    public static double kSAlgaeSim = 0.2;
+    public static double kVAlgaeSim = 1.98;
     public static double kAAlgaeSim = 0.0;
 
-    public static double kSCoralSim = 0.0;
-    public static double kVCoralSim = 0.0;
+    public static double kSCoralSim = 2.0;
+    public static double kVCoralSim = 0.4;
     public static double kACoralSim = 0.0;
 
-    public static double kSScoringSim = 0.0;
-    public static double kVScoringSim = 0.0;
+    public static double kSScoringSim = 0.1;
+    public static double kVScoringSim = 2.1;
     public static double kAScoringSim = 0.0;
 
     
--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/sim/RollerSim.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/sim/RollerSim.java
@@ -17,8 +17,22 @@ public class RollerSim implements RollerIO {
         double momentOfInertia = 0.04;
         double measurementNoise = 0.0;
 
+        // calculates how angular velocity changes over time with applied voltage
         LinearSystem<N1, N1, N1> flyWheelSystem = LinearSystemId.createFlywheelSystem(motor, gearRatio, momentOfInertia);
         rollerMotor = new FlywheelSim(flyWheelSystem, motor, measurementNoise);
     }
     
+    @Override
+    public void updateData(RollerData data) {
+        rollerMotor.update(0.02);
+        data.rollerVelocityRadPerSec = rollerMotor.getAngularVelocityRadPerSec();
+        data.currentAmps = rollerMotor.getCurrentDrawAmps();
+        data.rollerTempCelcius = 0.0;
+        // data.sensorTripped = data.rollerVelocityRadPerSec > 12.0;
+    }
+
+    @Override
+    public void setVoltage(double rollerVolts) {
+        rollerMotor.setInputVoltage(rollerVolts);
+    }
 }
