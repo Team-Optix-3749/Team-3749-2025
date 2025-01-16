f7a6af32a90d025d732e931646b7b0272ffb6a6b
LiliWuu <lilianwu08@gmail.com>
2025-01-16 12:30:47 -0800

    logging roller

--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/Roller.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.roller;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.roller.RollerIO.RollerData;
+import frc.robot.utils.ShuffleData;
 import frc.robot.subsystems.roller.RollerConstants.RollerStates;
 
 public abstract class Roller extends SubsystemBase {
@@ -12,6 +13,11 @@ public abstract class Roller extends SubsystemBase {
     private final PIDController positionController;
     private double lastKnownVelocity = 0.0;
 
+    private ShuffleData<Double> RollerVelocityLog = new ShuffleData<Double>(this.getName(), "roller velocity", 0.0);
+    private ShuffleData<Double> RollerVoltageLog = new ShuffleData<Double>(this.getName(), "roller voltage", 0.0);
+    private ShuffleData<Double> RollerCurrentLog = new ShuffleData<Double>(this.getName(), "roller current", 0.0);
+    private ShuffleData<String> StateLog = new ShuffleData<String>(this.getName(), "state", RollerStates.STOP.name());
+
     public Roller(RollerIO rollerIO) {
         this.rollerIO = rollerIO;
         this.positionController = new PIDController(0.1, 0, 0);
@@ -20,6 +26,10 @@ public abstract class Roller extends SubsystemBase {
     
     public void setVoltage(double volts) {
         rollerIO.setVoltage(volts);
+    }
+
+    public RollerStates getState() {
+        return rollerStates;
     }
 
     public void setState(RollerStates rollerStates) {
@@ -54,6 +64,10 @@ public abstract class Roller extends SubsystemBase {
     @Override
     public void periodic() {
         rollerIO.updateData(rollerData);
+        RollerVelocityLog.set(rollerData.rollerVelocityRadPerSec);
+        RollerVoltageLog.set(rollerData.rollerVolts);
+        RollerCurrentLog.set(rollerData.currentAmps);
+        StateLog.set(rollerStates.name());
     }
 
 }
