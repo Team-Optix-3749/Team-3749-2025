f77ea23d616d462a45c76d694cb54a33fe8ecb91
LiliWuu <lilianwu08@gmail.com>
2025-01-18 15:35:13 -0800

    fixed constants + added enums

--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/RollerConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/RollerConstants.java
@@ -1,30 +1,60 @@
 package frc.robot.subsystems.roller;
-
 public class RollerConstants {
-    public static final int algaeMotorId = 1;
-    public static final int coralMotorId = 2; 
-    public static final int scoringMotorId = 3; 
+    public static final class Algae {
+        public static final int motorId = 1;
+        public static final double momentOfInertia = 0.04;
+        public static final double gearRatio = 1.0;
+        public static final double measurementNoise = 0.0;
 
-    public static final double momentOfInertia = 0.04;
-    public static final double gearRatio = 1.0;
-    public static final double measurementNoise = 0.0;
+        public static final double kPSim = 10.0;
+        public static final double kISim = 0.0;
+        public static final double kDSim = 0.0;
+        public static final double kSSim = 0.0;
+        public static final double kVSim = 0.02;
+        public static final double kASim = 0.0;
 
-    // PID and FF values
-    public static final double kPSim = 10.0;
-    public static final double kISim = 0.0;
-    public static final double kDSim = 0.0;
-    public static final double kSSim = 0.0;
-    public static final double kVSim = 0.02;
-    public static final double kASim = 0.0;
+        public static final double velocity = 5.0;
+    }
+    public static final class Coral {
+        public static final int motorId = 2; 
+        public static final double momentOfInertia = 0.04;
+        public static final double gearRatio = 1.0;
+        public static final double measurementNoise = 0.0;
 
-    public static final double coralVelocity = 7.0;
-    public static final double algaeVelocity = 5.0;
-    public static final double scoringVelocity = 10.0;
-    public static final double holdVoltage = 2.0;
+        public static final double kPSim = 10.0;
+        public static final double kISim = 0.0;
+        public static final double kDSim = 0.0;
+        public static final double kSSim = 0.0;
+        public static final double kVSim = 0.02;
+        public static final double kASim = 0.0;
 
+        public static final double velocity = 7.0;
+    }
+    public static final class Scoring {
+        public static final int motorId = 3; 
+        public static final double momentOfInertia = 0.04;
+        public static final double gearRatio = 1.0;
+        public static final double measurementNoise = 0.0;
+
+        public static final double kPSim = 10.0;
+        public static final double kISim = 0.0;
+        public static final double kDSim = 0.0;
+        public static final double kSSim = 0.0;
+        public static final double kVSim = 0.02;
+        public static final double kASim = 0.0;
+
+        public static final double velocity = 10.0;
+    }
+    
     public enum RollerStates {
         RUN,
         MAINTAIN,
         STOP
+    }
+
+    public enum Implementations {
+        CORAL,
+        ALGAE,
+        SCORING
     }
 }
